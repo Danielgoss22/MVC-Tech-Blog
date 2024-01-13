dd5054c1475920f6e8bf6c59484dcf2107ee889e
aalyman42 <aalyman42@gmail.com>
2024-01-13 10:46:20 -0700

    added hooks to lowercase email and encrypt password for user creation and update.

--- models/Users.js
+++ models/Users.js
@@ -1,6 +1,6 @@
 const { Model, DataTypes } = require("sequelize");
 const sequelize = require("../config/connection");
-
+const bcrypt = require("bcrypt");
 class Users extends Model {}
 
 Users.init(
@@ -28,6 +28,21 @@ Users.init(
     },
   },
   {
+    hooks: {
+      beforeCreate: async (newUserData) => {
+        newUserData.email = await newUserData.email.toLowerCase();
+        newUserData.password = await bcrypt.hash(newUserData.password, 10);
+        return newUserData;
+      },
+      beforeUpdate: async (updatedUserData) => {
+        updatedUserData.email = await updatedUserData.email.toLowerCase();
+        updatedUserData.password = await bcrypt.hash(
+          updatedUserData.password,
+          10
+        );
+        return updatedUserData;
+      },
+    },
     sequelize,
     modelName: "users",
   }
