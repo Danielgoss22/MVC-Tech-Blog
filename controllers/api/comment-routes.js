e07b83dd07719246eb5a6b6ff8919d59e1810751
aalyman42 <aalyman42@gmail.com>
2024-01-15 13:51:47 -0700

    post and comments pull user id from session

--- controllers/api/comment-routes.js
+++ controllers/api/comment-routes.js
@@ -4,12 +4,11 @@ const withAuth = require("../../utils/auth");
 
 router.post("/", withAuth, async (req, res) => {
   try {
+    const userId = req.session.user_id;
     const newComment = await Comments.create({
-      where: {
-        user_id: req.session.user_id,
-        post_id: req.body.post_id,
-        comment_body: req.body.comment_body,
-      },
+      user_id: userId,
+      post_id: req.body.post_id,
+      comment_body: req.body.comment_body,
     });
 
     res.status(200).json(newComment);
