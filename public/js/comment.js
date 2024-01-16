ff1e75178c221e43427f9fb35ac0f950077c7302
Chance Creger <chance.creger@gmail.com>
2024-01-16 10:56:07 -0700

    added logic to comment add

--- public/js/comment.js
+++ public/js/comment.js
@@ -1,24 +1,29 @@
-const addComment = async (event) => {
-  event.preventDefault();
+document.addEventListener("DOMContentLoaded", function () {
+  const form = document.querySelector("form");
+  // const addCommentButton = document.getElementById("add-comment");
 
-  const string = window.location.toString();
-  const id = string[4];
-  const comment = document.getElementById("newComment").value.trim();
-  if (post_body) {
-    const response = await fetch(`/api/comments/${id}`, {
+  form.addEventListener("submit", function (event) {
+    event.preventDefault();
+
+    const comment = document.getElementById("newComment").value;
+
+    const newComment = {
+      comment_body: comment,
+    };
+
+    fetch("/api/comments/", {
       method: "POST",
-      body: JSON.stringify({ comment }),
-      header: { "Content-Type": "application/json" },
-    });
-    if (response.ok) {
-      alert("Comment added");
-      document.location.replace(`/api/comments/${id}`);
-    } else {
-      alert(response.statusText);
-    }
-  }
-};
-
-const addCommentBtn = document.getElementById("add-comment");
-
-addCommentBtn.addEventListener("submit", addComment);
+      headers: { "Content-Type": "application/json" },
+      body: JSON.stringify(newComment),
+    })
+      .then((response) => response.json())
+      .then((data) => {
+        console.log("Comment added:", data);
+        alert("Comment Added");
+      })
+      .catch((error) => {
+        console.error("Error adding comment:", error);
+        alert("Error adding comment. Please try again later");
+      });
+  });
+});
